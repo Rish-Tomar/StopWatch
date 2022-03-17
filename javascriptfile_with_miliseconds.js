02d5ebf5b18c8bc995d7fff96716f9e2166f6ba0
Rish-Tomar <101789466+Rish-Tomar@users.noreply.github.com>
2022-03-17 17:23:20 +0530

    Add files via upload

--- javascriptfile_with_miliseconds.js
+++ javascriptfile_with_miliseconds.js
@@ -1,41 +1,52 @@
 //variable declaration and initialixzation
 let sec=0;
 let min=0;
-
+let milisec=0;
 
 //boolean variable to monitor start and stop button
 let boolean=0;
 
 //Stopwatch logic function
 function logic()
-{		
-	sec++;
-	if(sec/60 ===1)
+{
+	milisec++;
+	if(milisec===250)
 	{
-	 	sec=0;
-	  min++;
-	  if(min /60 ===1)
-	  {
-	   	min=0;
-	    hour++;
-	  }
-	}
-    
+		milisec=0;
+	    sec++;
+	    if(sec/60 ===1)
+	    {
+	    	sec=0;
+		    min++;
+		    if(min /60 ===1)
+		    {
+		    	min=0;
+			    hour++;
+		    }
+	    }
+    }
+let dispmsec=null;
+ //digit correction for miliseconds
+   if(milisec<10)
+   {
+   	dispmsec="0"+milisec.toString();
+   }
+   else
+   	dispmsec=milisec;
+   
  //digit correction for seconds
 
-  if(sec<10)
+   if(sec<10)
    	dispsec="0"+sec.toString();
-  else 
+   else 
    	dispsec=sec;
-  
-  //minute correction
-  if(min<10)
-   	dispmin="0"+min.toString();
-   else
-   	dispmin=min;
-  	
-	document.getElementById("minutes").innerHTML=dispmin;
-	document.getElementById("seconds").innerHTML=dispsec;	
+
+    
+	
+	document.getElementById("minutes").innerHTML=min;
+	document.getElementById("seconds").innerHTML=dispsec;
+	document.getElementById("milisec").innerHTML=dispmsec;
+	
 }
 
 var timer=null;
@@ -45,7 +56,7 @@ function start(){
 
 	if(boolean==0)
 	{
-       timer=setInterval(logic,1000);
+       timer=setInterval(logic,1);
        boolean=1;
 	}
 
@@ -69,5 +80,6 @@ function reset(){
 	min=0;
 	document.getElementById("seconds").innerHTML="00";
 	document.getElementById("minutes").innerHTML="00";
+	document.getElementById("milisec").innerHTML="00";
 	boolean=0;
 }
